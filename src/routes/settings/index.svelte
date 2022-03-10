544475f8da64c364584b53eed69ba08554186a65
Felix M <felix.p.magnus@gmail.com>
2022-03-10 17:30:53 +0100

    make settings more beautiful

--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -23,15 +23,38 @@
 </svelte:head>
 
 <PageTitle title="Einstellungen" />
-<input type="text" bind:value={schoolClassInput} />
-<button on:click={addSchoolClass}>add class</button>
-<ul>
-	{#each $schoolClasses as schoolClass}
-		<li>
-			<div style="display: flex; flex-direction: row; gap: 1rem;">
-				<p>{schoolClass}</p>
-				<button on:click={() => deleteSchoolClass(schoolClass)}>del</button>
-			</div>
-		</li>
-	{/each}
-</ul>
+<div class="max-w-xs">
+	<label class="block text-gray-700 text-sm font-bold mt-8 mb-2" for="newClassInput">
+		Klassen
+	</label>
+
+	<ul>
+		{#each $schoolClasses as schoolClass}
+			<li>
+				<div class="flex flex-row gap-4 justify-between">
+					<p>{schoolClass}</p>
+					<button
+						class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
+						on:click={() => deleteSchoolClass(schoolClass)}>-</button
+					>
+				</div>
+			</li>
+		{/each}
+	</ul>
+
+	<label class="block text-gray-700 text-sm font-bold mt-8 mb-2" for="newClassInput">
+		Neue Klasse hinzufügen
+	</label>
+	<div class="flex flex-row gap-4 justify-between" id="newClassInput">
+		<input
+			class="w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+			type="text"
+			placeholder="Klassenname..."
+			bind:value={schoolClassInput}
+		/>
+		<button
+			class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+			on:click={addSchoolClass}>+</button
+		>
+	</div>
+</div>
