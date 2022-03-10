007715f1169c7e31c8d1374717ed43501f2b7ff7
Felix M <felix.p.magnus@gmail.com>
2022-03-11 00:29:48 +0100

    make class list drag-droppable

--- src/components/ClassList.svelte
+++ src/components/ClassList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { flip } from 'svelte/animate';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { schoolClasses } from '../store';
@@ -10,15 +11,57 @@
 			)
 		);
 	};
+
+	let draggingIndex: number | null = null;
+	let hoveringIndex: number | null = null;
+
+	const drop = (event: DragEvent, target: number) => {
+		event.preventDefault();
+
+		draggingIndex = null;
+
+		event.dataTransfer.dropEffect = 'move';
+
+		const start = parseInt(event.dataTransfer.getData('text/plain'));
+		const newTracklist = $schoolClasses;
+
+		if (start < target) {
+			newTracklist.splice(target + 1, 0, newTracklist[start]);
+			newTracklist.splice(start, 1);
+		} else {
+			newTracklist.splice(target, 0, newTracklist[start]);
+			newTracklist.splice(start + 1, 1);
+		}
+		schoolClasses.set(newTracklist);
+		hoveringIndex = null;
+	};
+
+	const dragstart = (event: DragEvent, index: number) => {
+		draggingIndex = index;
+		event.dataTransfer.effectAllowed = 'move';
+		event.dataTransfer.dropEffect = 'move';
+		event.dataTransfer.setData('text/plain', `${index}`);
+	};
 </script>
 
 <label class="block text-gray-700 text-sm font-bold mb-2" for="newClassInput"> Klassen </label>
 
-<ul>
-	{#each $schoolClasses as schoolClass}
-		<li
-			class:bg-slate-200={$page.params.schoolClass === schoolClass}
+<div>
+	{#each $schoolClasses as schoolClass, index (schoolClass)}
+		<div
 			class="border-2 rounded-l p-3 mb-4"
+			class:bg-slate-200={$page.params.schoolClass === schoolClass}
+			class:bg-white={draggingIndex === index}
+			class:bg-blue-500={hoveringIndex === index}
+			draggable={true}
+			animate:flip
+			on:dragstart={(event) => dragstart(event, index)}
+			on:drop={(event) => drop(event, index)}
+			on:dragover={(event) => event.preventDefault()}
+			on:dragenter={(event) => {
+				event.preventDefault();
+				hoveringIndex = index;
+			}}
 		>
 			<div class="flex flex-row gap-4 justify-between items-center">
 				<p class="text-xl text-gray-700">{schoolClass}</p>
@@ -35,6 +78,6 @@
 					on:click={() => deleteSchoolClass(schoolClass)}>-</button
 				>
 			</div>
-		</li>
+		</div>
 	{/each}
-</ul>
+</div>
